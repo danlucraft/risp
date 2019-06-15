65a017d48986e50ca1abb53fb622f7aed4f64575
Dan Lucraft <dan@lucraft.me>
2019-06-15 20:26:00 +0100

    Spot list start

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use std::io::{stdin, Write, stdout};
 
+#[derive(Debug)]
+enum Exp {
+    Atom(String),
+    List(Vec<Exp>)
+}
+
 fn read_line() -> String {
     print!(">> ");
     stdout().flush().expect("couldn't flush stdout");
@@ -8,7 +14,22 @@ fn read_line() -> String {
     line
 }
 
+fn parse_expression(line: &String) -> Result<Exp, String> {
+    for ch in line.chars() {
+        if ch == '(' {
+            println!("Spotted list start");
+        } else {
+            return Err("Malformed expression".to_owned())
+        }
+    }
+    Ok(Exp::Atom("asd".to_owned()))
+}
+
 fn main() {
     let line = read_line();
-    println!("{}", line)
+    println!("{}", line);
+    let exp = parse_expression(&line);
+    println!("{:?}", exp);
 }
+
+
