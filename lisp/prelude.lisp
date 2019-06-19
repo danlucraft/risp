298fee5cd503b967fe936ffae9da3ed320fb04eb
Daniel Lucraft <dan@lucraft.me>
2019-06-19 22:13:00 +0100

    Some prelude functions

--- lisp/prelude.lisp
+++ lisp/prelude.lisp
@@ -1,3 +1,15 @@
 (defun nth (n list)
   (cond (eq n 0) (car list)
         true     (nth (- n 1) (cdr list))))
+
+(defun null? (x)
+  (eq x '()))
+
+(defun and (x y)
+  (cond x    (cond y    true
+                   true false)
+        true false))
+
+(defun not (x)
+  (cond x    false
+        true true))
